821c583d4a5867689bf1dbb3b6e3558f9a19db08
João Gabriel Dourado Cervo <joaogabrieldouradocervo@gmail.com>
2021-02-19 10:45:08 -0300

    Envio inicial dos arquivos

---
+++ 100DaysOfSwift-Project11/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  100DaysOfSwift-Project11
+//
+//  Created by João Gabriel Dourado Cervo on 19/02/21.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+}
